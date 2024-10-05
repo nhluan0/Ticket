6fe50acd9a25c64ea5a9fdef96a44ea92f3901e2
luan nguyen <nhluan0@gmail.com>
2024-10-05 15:34:28 +0700

    add first

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import User from './component/User'
+import { dataAdmin, dataNotYet, dataUser } from './component/Data'
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="text-uppercase my-4 text-center">
+        Demo Some image srceen project Lottery Tickets
+      </h1>
+      <User
+        title="Main Functions for user not yet register"
+        data={dataNotYet}
+      />
+      <br></br>
+
+      <User title="Main Function for user has roles user" data={dataUser} />
+      <br></br>
+
+      <User title="Main Function for admin" data={dataAdmin} />
+    </div>
+  )
+}
+
+export default App
---
+++ src/component/Data.js
@@ -0,0 +1,92 @@
+export const dataNotYet = [
+  {
+    url: '/filter_by_area_or_name_lottery.png',
+    title: 'filter_by_area_or_name_lottery',
+  },
+
+  {
+    url: '/filter_by_name_lottery_and_date.png',
+    title: 'filter_by_name_lottery_and_date',
+  },
+  {
+    url: '/find_result_lottery.png',
+    title: 'find_result_lottery',
+  },
+  {
+    url: '/langding_page.png',
+    title: 'langding_page',
+  },
+  {
+    url: '/Login.png',
+    title: 'Login',
+  },
+  {
+    url: '/register.png',
+    title: 'register',
+  },
+  {
+    url: '/View_Result_Find_Lottory.png',
+    title: 'View_Result_Find_Lottory',
+  },
+]
+export const dataUser = [
+  {
+    url: '/delete_history_find_lottery.png',
+    title: 'delete_history_find_lottery',
+  },
+
+  {
+    url: '/history_find_result_lottery.png',
+    title: 'history_find_result_lottery',
+  },
+  {
+    url: '/login_success.png',
+    title: 'login_success',
+  },
+  {
+    url: '/forget_password.png',
+    title: 'forget_password',
+  },
+  {
+    url: '/change_password.png',
+    title: 'change_password',
+  },
+]
+export const dataAdmin = [
+  {
+    url: '/Manager_Lottery_by_name_area.png',
+    title: 'Manager_Lottery_by_name_area',
+  },
+  {
+    url: '/manger_user.png',
+    title: 'manger_user',
+  },
+  {
+    url: '/manager_result_lotery_ticket.png',
+    title: 'manager_result_lotery_ticket',
+  },
+  {
+    url: '/manager_history_find_result.png',
+    title: 'manager_history_find_result',
+  },
+  {
+    url: '/delete_less_or_delete_more.png',
+    title: 'delete_less_or_delete_more',
+  },
+  {
+    url: '/reset_password.png',
+    title: 'reset_password.png',
+  },
+  {
+    url: '/add_update_result_lottery_ticket.png',
+    title: 'add_update_result_lottery_ticket',
+  },
+  {
+    url: '/add_update_user.png',
+    title: 'add_update_user',
+  },
+  {
+    url: '/add_lottery.png',
+    title: 'add_lottery',
+  },
+]
